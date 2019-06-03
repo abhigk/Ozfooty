608951f280377625b8d196b58fb145731860cb7d
abhishekgupta0595@gmail.com <abhishekgupta0595@gmail.com>
2019-06-03 23:08:06 +1000

    added node prod port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const https = require('https');
 var HTMLParser = require('node-html-parser');
 var html2json = require('html2json').html2json;
@@ -151,8 +151,3 @@ app.get("/getTopPlayers", function (req, res) {
 //      }
 //      return true;
 //  }
-
-
-
-
-
